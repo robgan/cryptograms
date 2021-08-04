0553fdbb69a6dc261e209057550b906898059496
Robert Gan <robgan503@gmail.com>
2021-08-03 20:46:20 -0400

    trying to deploy

--- src/App.js
+++ src/App.js
@@ -204,46 +204,6 @@ function App() {
         });
     }
 
-    // function postToTwitter(){
-    // const OAuth = require('oauth');
-    //
-    // const twitter_application_consumer_key = '';  // API Key
-    // const twitter_application_secret = '';  // API Secret
-    // const twitter_user_access_token = '1068275224246927361-';  // Access Token
-    // const twitter_user_secret = '';  // Access Token Secret
-    //
-    // const oauth = new OAuth.OAuth(
-    //     'https://api.twitter.com/oauth/request_token',
-    //     'https://api.twitter.com/oauth/access_token',
-    //     twitter_application_consumer_key,
-    //     twitter_application_secret,
-    //     '1.0A',
-    //     null,
-    //     'HMAC-SHA1'
-    // );
-    //
-    // const status = "";
-    // // let status = "I deciphered a quote playing Cryptograms;  // This is the tweet (ie status)
-    //
-    // const postBody = {
-    //     'status': status
-    // }
-    //
-    // // console.log('Ready to Tweet article:\n\t', postBody.status);
-    // oauth.post('https://api.twitter.com/1.1/statuses/update.json',
-    //     twitter_user_access_token,  // oauth_token (user access token)
-    //     twitter_user_secret,  // oauth_secret (user secret)
-    //     postBody,  // post body
-    //     '',  // post content type ?
-    //     function(err, data, res) {
-    //         if (err) {
-    //             console.log(err);
-    //         } else {
-    //             // console.log(data);
-    //         }
-    //     });
-    // }
-
     return (
         <div className={"cryptograms"}>
             <div className={"header"}>
